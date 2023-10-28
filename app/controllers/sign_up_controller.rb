adde56690480953bf5702bcc94f4734759e1a136
Angga Jiyan <anggajiyanfajar@gmail.com>
2023-10-28 07:38:53 +0700

    add homepage views and controller

--- app/controllers/sign_up_controller.rb
+++ app/controllers/sign_up_controller.rb
@@ -5,7 +5,7 @@ class SignUpController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save 
-            redirect_to signup_path
+            redirect_to root_path
         else
             render :index, :status => 404
         end
